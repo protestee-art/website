6e2867e4968a9cfbd1ed0d0eff5202f833197ba5
Shahidh K Muhammed <shahidh@hasura.io>
2020-01-22 03:11:10 +0530

    convert add to cart a class component

--- src/components/Shelf/ProductList/Product/index.js
+++ src/components/Shelf/ProductList/Product/index.js
@@ -6,63 +6,87 @@ import Thumb from '../../../Thumb';
 import { formatPrice } from '../../../../services/util';
 import { addProduct } from '../../../../services/cart/actions';
 
-const Product = ({ product, addProduct }) => {
-  product.quantity = 1;
+class Product extends React.Component {
+  static propTypes = {
+    product: PropTypes.object.isRequired,
+    addProduct: PropTypes.func.isRequired,
+  }
 
-  let formattedPrice = formatPrice(product.price, product.currencyId);
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: this.props.product,
+      size: ''
+    };
+  }
 
-  let productInstallment;
+  handleAddProduct = () => {
+    const { product, size } = this.state;
+    product.size = size;
+    console.log(product);
+    this.props.addProduct(product);
+  }
 
-  if (!!product.installments) {
-    const installmentPrice = product.price / product.installments;
+  handleSizeChange = (size) => {
+    this.setState({size});
+  }
 
-    productInstallment = (
-      <div className="installment">
-        <span>or {product.installments} x</span>
-        <b>
-          {product.currencyFormat}
-          {formatPrice(installmentPrice, product.currencyId)}
-        </b>
+  render () {
+
+    const { product } = this.state;
+
+    product.quantity = 1;
+
+    let formattedPrice = formatPrice(product.price, product.currencyId);
+
+    let productInstallment;
+
+    if (!!product.installments) {
+      const installmentPrice = product.price / product.installments;
+
+      productInstallment = (
+        <div className="installment">
+          <span>or {product.installments} x</span>
+          <b>
+            {product.currencyFormat}
+            {formatPrice(installmentPrice, product.currencyId)}
+          </b>
+        </div>
+      );
+    }
+
+    return (
+      <div
+        className="shelf-item"
+        data-sku={product.sku}
+      >
+        {product.isFreeShipping && (
+          <div className="shelf-stopper">Free shipping</div>
+        )}
+        <Thumb
+          classes="shelf-item__thumb"
+          src={require(`../../../../static/products/${product.sku}_1.jpg`)}
+          alt={product.title}
+        />
+        <p className="shelf-item__title">{product.title}</p>
+        <div className="shelf-item__price">
+          <div className="val">
+            <small>{product.currencyFormat}</small>
+            <b>{formattedPrice.substr(0, formattedPrice.length - 3)}</b>
+            <span>{formattedPrice.substr(formattedPrice.length - 3, 3)}</span>
+          </div>
+          <select name="size" onChange={(e)=>{this.handleSizeChange(e.target.value)}}> 
+            {product.availableSizes.map((size) => <option value={size}>{size}</option> )}
+          </select>
+          {productInstallment}
+        </div>
+        <div 
+          className="shelf-item__buy-btn"
+          onClick={this.handleAddProduct}
+        >Add to cart</div>
       </div>
     );
   }
-
-  return (
-    <div
-      className="shelf-item"
-      data-sku={product.sku}
-    >
-      {product.isFreeShipping && (
-        <div className="shelf-stopper">Free shipping</div>
-      )}
-      <Thumb
-        classes="shelf-item__thumb"
-        src={require(`../../../../static/products/${product.sku}_1.jpg`)}
-        alt={product.title}
-      />
-      <p className="shelf-item__title">{product.title}</p>
-      <div className="shelf-item__price">
-        <div className="val">
-          <small>{product.currencyFormat}</small>
-          <b>{formattedPrice.substr(0, formattedPrice.length - 3)}</b>
-          <span>{formattedPrice.substr(formattedPrice.length - 3, 3)}</span>
-        </div>
-        <select> 
-          {product.availableSizes.map((size) => <option value={size}>{size}</option> )}
-        </select>
-        {productInstallment}
-      </div>
-      <div 
-        className="shelf-item__buy-btn"
-        onClick={() => addProduct(product)}
-      >Add to cart</div>
-    </div>
-  );
-};
-
-Product.propTypes = {
-  product: PropTypes.object.isRequired,
-  addProduct: PropTypes.func.isRequired
 };
 
 export default connect(
